cef8b1bb6d58c61edc008bba43b4964beda5fb5a
Hernán D. Mercado <hernandmf@gmail.com>
2021-06-29 13:02:24 -0500

    first commit

---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+const Home = ()=>{
+  const view = `
+  <div class="Character">
+    <article class="Character-item">
+      <az href = "#/1/">
+        <img src = "image" alt = "name">
+        <h2>Name</h2>
+      </az>
+    </article>
+  </div>
+  `;
+  return view;
+}
+export default Home;
