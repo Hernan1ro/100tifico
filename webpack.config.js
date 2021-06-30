ce5357726eeaae5f67b63c03bd1f09c71d978fa0
Hernán D. Mercado <hernandmf@gmail.com>
2021-06-30 00:58:18 -0500

    ready to deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: ["@babel/polyfill", "./src/index.js"],
@@ -33,6 +34,14 @@ module.exports = {
       inject: true,
       template: "./public/index.html",
       filename: "./index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/styles/styles.css"),
+          to: "",
+        },
+      ],
     }),
   ],
 };
